5ed14c8ff74d3765e9e5955ea27e518646112d19
akhilsharmaa <akhil.console@gmail.com>
2021-09-12 20:07:05 +0530

    Passed :) & Accepted by LeetCode

--- LeetCode/Container With Most Water/Solution.cpp
+++ LeetCode/Container With Most Water/Solution.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
+    // Passed :) & Accepted by LeetCode
     int maxArea(vector<int>& height) {
         int left = 0;
-        int right = height.size() -1;
+        int right = height.size() - 1;
+        int water = 0;
+
+        while(left != right){
+            int distance = right - left;
+
+            if (height[left] < height[right] ){
+
+                water = max(height[left] * distance , water);
+
+                left++;
+            }else{
+                water = max(height[right] * distance , water);
+                right--;
+            }
+            cout<< water << ", ";
+        }
+
+        return water;          
+    }
+
+    // In This i havn't Used Maths Library of Cpp.
+    int maxArea2(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
         int water = 0;
 
         while(left != right){
