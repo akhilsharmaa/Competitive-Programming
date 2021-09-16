e838fad4a7cff04d3d59d13d40315af62215ba9b
akhilsharmaa <akhil.console@gmail.com>
2021-09-16 07:34:14 +0530

    Accepted By CodeChief.

---
+++ CodeChef/Find Remainder/FindReminder.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// ACCEPTED By Code Chief:)
+int main(){
+
+    // Enter Number of Inputs
+	int a; 
+	cin >> a;
+
+	  for(int i = 0 ; i < a; i++ ){
+        int input1, input2; 
+        cin >> input1 >> input2 ;
+
+        cout<< input1 % input2 <<endl;
+
+      }
+	
+
+
+
+
+   return 0;
+}
