51b30a5910844508088b1f85b3410e5200b90b75
akhilsharmaa <akhil.console@gmail.com>
2021-09-23 16:25:24 +0530

    Solved Roman to Integer

---
+++ LeetCode/Roman To Integer/Solution.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+ // ACEPTED :) 
+class Solution {
+public:
+    int romanToInt(string s) {
+
+        map<char, int> con;
+        con['I'] =  1;
+        con['V'] =  5;
+        con['X'] =  10;
+        con['L'] =  50;
+        con['C'] =  100;
+        con['D'] =  500;
+        con['M'] =  1000;
+
+        char lastEl = s[s.length() -1] ;  
+        int total = con[lastEl];
+
+        cout<< con[2] <<endl;
+
+        for (int i = s.length() -2; i >= 0 ; i--){
+            if ( con[s[i]] < con[s[i + 1]] ){
+                total -= con[s[i]];
+            }else{
+                total += con[s[i]];
+            }
+        }
+
+        // Returning the Interger value of roman 
+        return total ;
+    }
+};
+
+int main(){
+
+    
+    Solution s ;
+    s.romanToInt("IV");
+
+
+   return 0;
+}
