e3294eb6237c05ebafacbea73546c08d4884d413
akhilsharmaa <akhil.console@gmail.com>
2021-10-01 18:50:47 +0530

    ACCEPTED

---
+++ HackerRank/Insert At Nth Position/Solution.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+
+class SinglyLinkedListNode {
+    public: 
+    int data;
+    SinglyLinkedListNode* next;
+    SinglyLinkedListNode(int val){
+        data = val;
+    }
+};
+
+// ACCEPTED :) 
+SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* llist, int data, int position) {
+    
+      SinglyLinkedListNode*node = new SinglyLinkedListNode(data);
+    node -> data = data;
+
+    if (llist == NULL) {
+        return node;
+    }
+    else {
+        SinglyLinkedListNode *last = llist;
+        int p = 0;
+
+        if (position == 0) {
+            node -> next = llist;
+            return node;
+        }
+
+        while (last != NULL) {
+            if (p == position - 1) {
+                node -> next = last -> next;
+                last -> next = node;
+            } else {
+                last = last -> next;
+            }
+            p = p + 1;
+        }
+        return llist; 
+    }
+    
+}
+
+int main(){
+
+}    
+
+
+   return 0;
+}
