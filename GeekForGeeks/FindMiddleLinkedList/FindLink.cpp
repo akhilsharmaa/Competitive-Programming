e8610c5ec6a4d7ea6c403263e97fd629bb1bdeec
akhilsharmaa <akhil.console@gmail.com>
2021-10-14 16:49:29 +0530

    ACCEPTED :)

---
+++ GeekForGeeks/FindMiddleLinkedList/FindLink.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+// ACCEPTED :) 
+class Solution{
+    public:
+    int getMiddle(Node *head){
+
+        // Two Pointers 
+        Node *temp = head, *temp_mid = head;
+
+        int count = 0;
+        while (temp -> next != NULL){
+
+            if (count % 2 == 0){
+                temp_mid = temp_mid -> next;
+            }
+
+            count ++;
+            temp = temp -> next;
+        }
+        
+        return temp_mid-> data;
+    }
+};
+
+
+int main(){
+
+    
+
+
+   return 0;
+}
