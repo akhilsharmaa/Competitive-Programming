ef1d01156f3fabfba5725e57e2ca21e925ec2ad4
akhilsharmaa <akhil.console@gmail.com>
2021-10-26 19:41:31 +0530

    ACCEPTED :)

---
+++ LeetCode/B-Tree Pre-order Traversa/Solution.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+
+// ACCEPTED :) 
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> v;
+        if(root == NULL) return v;
+
+        st.push(root);
+        while (!st.empty()){
+            auto a = st.top();
+            st.pop();
+            v.push_back(a->val);
+
+            if(a->right){st.push(a->right);}
+            if(a->left){st.push(a->left);}
+        }
+        
+        return v;
+    }
+};
