ea0abe1d07a9100c15e0176e4ce598f834803984
akhilsharmaa <akhil.console@gmail.com>
2021-10-30 20:01:05 +0530

    ACCEPTED :) SELECTION SORT

---
+++ GeekForGeeks/Selection Sort/SelectionSort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+// ACCEPTED :) 
+class Solution
+{
+    public:
+    int select(int arr[], int i)
+    {
+        // code here such that selectionSort() sorts arr[]
+    }
+     
+    void selectionSort(int arr[], int n)
+    {
+       
+
+    for (int i = 0; i < n; i++){
+        
+
+        int min = INT32_MAX;
+        int minIndex = 0;
+
+        for (int j = i; j < n; j++){
+
+            if(min > arr[j]) {
+                min = arr[j];
+                minIndex = j;
+            }
+        }
+        
+
+        arr[minIndex] = arr[i];
+        arr[i] = min;
+
+    }
+       
+    }
+};
+
+int main(){
+
+   return 0;
+}
