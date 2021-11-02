887f4b498336566cf159d2a3322ba6683538b094
akhilsharmaa <akhil.console@gmail.com>
2021-11-02 14:35:52 +0530

    ACCEPTED : ) 1 question of november

---
+++ GeekForGeeks/Is List Palindrom/paldromeList.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+// ACCEPTED :) 
+class Solution{
+  public:
+    bool isPalindrome(Node *head){
+
+        Node *temp =  head;
+        int a = 0, b = 0 , multiplies = 1;
+
+        while (temp != NULL){
+            a = (a * 10) + (temp-> data);
+            
+            // Collecting right to left
+            b = b + (temp->data  * multiplies);
+            multiplies = multiplies * 10;
+
+            temp = temp -> next;
+        }
+        
+        if(a == b) return true;
+        return false;
+    }
+};
+
+
+int main(){
+
+
+
+
+   return 0;
+}
