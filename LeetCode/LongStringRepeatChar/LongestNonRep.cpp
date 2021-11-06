ca0f654ff375c10d8d858cede44cac8c25ac6a70
akhilsharmaa <akhil.console@gmail.com>
2021-11-06 19:06:09 +0530

    Accepted

---
+++ LeetCode/LongStringRepeatChar/LongestNonRep.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#include <set>
+#include<hash_set>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+    
+        unordered_map<char, size_t> last_occurrence;
+
+        size_t starting_idx = 0, ans = 0;
+
+        for (size_t i = 0; i < s.size(); ++i) {
+            auto it(last_occurrence.find(s[i]));
+    
+            if (it == last_occurrence.cend()) {
+                last_occurrence.emplace_hint(it, s[i], i);
+            } else {  // s[i] appeared before. Check its validity.
+                if (it->second >= starting_idx) {
+                    ans = max(ans, i - starting_idx);
+                    starting_idx = it->second + 1;
+                }
+                it->second = i;
+            }
+
+        }
+        ans = max(ans, s.size() - starting_idx);
+        return ans;
+    }
+};
+
+int main(){
+
+    Solution s;
+    cout<<s.lengthOfLongestSubstring("pwwqkew");
+    return 0;
+}
