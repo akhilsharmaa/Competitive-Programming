c1fb900a7b147d735c7b94aba4c1476fc565c3b2
akhilsharmaa <akhilsharma3332@gmail.com>
2021-11-14 08:59:25 +0530

    Fnd peak  ele accepted

---
+++ LeetCode/Find Peak Element/findPeak.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {  
+        int left = 0, right = nums.size() - 1;
+
+        while (left < right){
+            int mid = left + (right -  left )/2;
+            if (nums[mid] < nums[mid + 1]){
+                left = mid + 1;
+            }else {
+                right = mid;
+            }
+        }
+        
+        return left;
+    }
+};
+
+int main(){
+
+    Solution s;
+
+    vector<int> v = {1,2,1,3,5,6,4};
+    cout<< s.findPeakElement(v);
+
+   return 0;
+}
