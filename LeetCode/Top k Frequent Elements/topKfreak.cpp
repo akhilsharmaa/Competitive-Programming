faa2f06bf86005c5b779733d2000c53ff1aadd25
akhilsharmaa <akhilsharma3332@gmail.com>
2021-11-20 08:17:13 +0530

    Top Fraequent K element.

---
+++ LeetCode/Top k Frequent Elements/topKfreak.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ACCEPTED :) 
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+     
+
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        
+
+        // MIN HEAP of MAP 
+        priority_queue <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > minHeap;
+
+        for (auto i = mp.begin() ; i != mp.end() ; i++){
+            minHeap.push({i->second, i->first});
+            if (minHeap.size() > k){
+                minHeap.pop();
+            }
+        }
+        
+        vector<int> vec;
+        for (int i = 0; i < k; i++){
+            vec.push_back(minHeap.top().second);
+            minHeap.pop();
+        }
+        
+
+        return vec;
+    }
+};
+int main(){
+   
+
+
+   return 0;
+}
