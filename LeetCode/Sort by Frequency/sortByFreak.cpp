34497fc02141428e8da07beae71449d6ac0f6b94
akhilsharmaa <akhilsharma3332@gmail.com>
2021-11-20 09:52:16 +0530

    Sort Array by Increasing Frequency

--- LeetCode/Sort by Frequency/sortByFreak.cpp
+++ LeetCode/Sort by Frequency/sortByFreak.cpp
@@ -1,8 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        
+       unordered_map<int, int> freq;
+        for(int &num: nums)
+            ++freq[num];
+        
+        sort(nums.begin(), nums.end(), [&](int a, int b) {
+            if(freq[a] == freq[b])
+                return a > b;
+            return freq[a] < freq[b];
+        });
+        
+        return nums;
+    }
+};
+
 int main(){
-   
+
+      Solution s; 
+      vector<int> v = {1,1,2,2,2,3};
+      vector<int> r  =  s.frequencySort(v);
+
+      for (auto &&i : r){
+         cout << i << " ";
+      }
+      
+
 
    return 0;
 }
