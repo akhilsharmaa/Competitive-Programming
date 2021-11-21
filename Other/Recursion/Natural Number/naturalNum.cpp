3a3c6ae5a84f8af74a218801c445fd7f43ff67f2
akhilsharmaa <akhilsharma3332@gmail.com>
2021-11-21 13:21:37 +0530

    Print in order of ( 5 4 3 2 1 1 2 3 4 5 )

--- Other/Recursion/Natural Number/naturalNum.cpp
+++ Other/Recursion/Natural Number/naturalNum.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 // Print natural no. via Recursive 
+// 10 9 8 7 6 5 4 3 2 1 1 2 3 4 5 6 7 8 9 10 
+void naturalNumAceDec(int n){
+    // Base case
+    if (n == 0)return;
+    cout<< n << " ";
+    // Self work
+    naturalNumAceDec(n -1);
+    cout<< n << " ";
+}
+
+
+// Print natural no. via Recursive 
+// 1 2 3 4 5 6 7 8 9 10 
 void naturalNum(int n){
     if(n == 0) return; // Base Case
     naturalNum(n - 1); // Self work
@@ -9,7 +22,7 @@ void naturalNum(int n){
 }
 
 int main(){
-   naturalNum(10);
+   naturalNumAceDec(10);
 
    return 0;
 }
