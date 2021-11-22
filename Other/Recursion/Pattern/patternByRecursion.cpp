420900db652278540bfa40145f63390e3f47fedc
akhilsharmaa <akhilsharma3332@gmail.com>
2021-11-22 08:17:28 +0530

    PRITING successfully

--- Other/Recursion/Pattern/patternByRecursion.cpp
+++ Other/Recursion/Pattern/patternByRecursion.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-/* // TODO: Print the pattern  
+/*  Print the pattern  
     # # # # # 
     # # # # 
     # # # 
@@ -10,13 +10,20 @@ using namespace std;
     # 
 */
 
-void func(){
-    
+void pattern1TODO:(int n, int i){
+    if (n > i){
+        cout << "# "; 
+        pattern1(n, i + 1);
+    }else{
+        cout<<endl;
+        pattern1(n - 1, 0);
+    }
 }
 
 
 int main(){
    
+   pattern1(6, 0);
 
    return 0;
 }
