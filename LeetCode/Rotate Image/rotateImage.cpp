8d49dea77dae212bc17d1abcd597238007a40eb7
akhilsharmaa <akhilsharma3332@gmail.com>
2021-11-23 22:22:19 +0530

    Rotate Image

---
+++ LeetCode/Rotate Image/rotateImage.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ACCEPTED :) 
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < i; j++)        
+                // SWAP THE NODES 
+                swap(matrix[i][j], matrix[j][i]);
+           
+    
+        for (int i = 0; i < n; i++)
+            reverse(matrix[i].begin(),matrix[i].end());
+        
+    }
+};
+
+int main(){
+   
+
+   return 0;
+}
