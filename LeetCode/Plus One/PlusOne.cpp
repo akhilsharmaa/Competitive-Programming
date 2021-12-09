bea2ad7448ba0aa1d2dbf856029b5c9e7979d6b0
akhilsharmaa <akhilsharma3332@gmail.com>
2021-12-09 15:33:04 +0530

    Plus One Accpeted :)

---
+++ LeetCode/Plus One/PlusOne.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ACCEPTED :) 
+// Runtime: 4 ms, faster than 44.89% of C++ online submissions for Plus One.
+// Memory Usage: 8.9 MB, less than 12.55% of C++ online submissions for Plus One.
+
+
+class Solution {
+public:
+
+void helperFunc(vector<int>& digits){
+   digits.push_back(0);
+   digits[0] = 1;
+} 
+
+vector<int> plusOne(vector<int>& digits) {
+        
+   int n = digits.size();
+   
+   if(digits[n - 1] != 9){
+      digits[n - 1] = (digits[n - 1] + 1);
+      return digits;
+   }
+
+   for (int i = n - 1; i >= 0; --i){
+         if(digits[i] != 9){
+            digits[i]++;
+            break;
+         }
+         else {
+            if (i == 0 && digits[0] == 9){
+               helperFunc(digits);
+            }else {
+               digits[i] = 0;
+            }
+         }        
+      }
+      return digits;
+   }
+};
+
+
+int main(){
+    
+    Solution s;
+    vector<int> v = {9, 9, 9};
+
+
+   cout << "\n Input : ";
+   for (auto &&i : v)
+      cout << i ;
+
+
+    vector<int> re =  s.plusOne(v);
+
+
+
+   cout << "\n Output : "; 
+   for (auto &&i : re)
+      cout << i ;
+   
+
+
+   return 0;
+}
