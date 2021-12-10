7b4af2fb18ca67956c9d0879d8da9b2d45ca5380
akhilsharmaa <akhilsharma3332@gmail.com>
2021-12-10 22:56:19 +0530

    completed day 5 challenge

---
+++ HackerRank/30 Days Of Code/Day 5: Loops/Loop.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Passes all testcases
+int main(){
+    
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= 10; i++){
+        cout << n << " x " << i << " = " << n * i << "\n";
+    }
+
+   return 0;
+}
