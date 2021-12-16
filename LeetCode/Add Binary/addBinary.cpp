313ee163848213b8b0d3d2652de6e38cea0772bb
akhilsharmaa <akhilsharma3332@gmail.com>
2021-12-16 20:42:17 +0530

    Solved :)

--- LeetCode/Add Binary/addBinary.cpp
+++ LeetCode/Add Binary/addBinary.cpp
@@ -20,13 +20,26 @@ public:
     int i=0,sum;
     
     while(i<a.size() || i<b.size()){
+
         sum= carry;
-        if(i<a.size()) sum+= a[i]-'0';
-        if(i<b.size()) sum+= b[i]-'0';
-        if(sum==0) carry=0, res+='0';
-        else if(sum==1)  carry=0, res+='1';
-        else if(sum==2)carry=1, res+='0';
+        
+        if(i<a.size())
+            sum+= a[i]-'0';
+        
+        if(i<b.size()) 
+            sum+= b[i]-'0';
+        
+        if(sum==0) 
+            carry=0, res+='0';
+       
+        else if(sum==1)
+            carry=0, res+='1';
+       
+        else if(sum==2)
+            carry=1, res+='0';
+       
         else carry=1, res+='1';
+
         i++;
     }
     if(carry==1)  res+='1';
