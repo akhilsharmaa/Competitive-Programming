7b7d73efcca9f6f8a61c3a025d0df678f1277a6c
akhilsharmaa <akhilsharma3332@gmail.com>
2021-12-17 21:03:16 +0530

    Single Num Solved

--- LeetCode/Single Number/single.cpp
+++ LeetCode/Single Number/single.cpp
@@ -2,10 +2,21 @@
 #include <vector>
 using namespace std;
 
+// Runtime: 40 ms, faster than 11.14% of C++ online submissions for Single Number.
+// Memory Usage: 17 MB, less than 48.80% of C++ online submissions for Single Number.
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
+        int n = nums.size();
+        int  a = 0;
+
+        for (int i = 0; i < n; i++){
+            a = a ^ nums[i];
+        }   
+
+        return a;
     }
 };
 
