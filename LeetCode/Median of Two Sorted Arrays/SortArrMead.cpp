7b6aea899376092072bbb34e14ff4245aa488ed2
akhilsharmaa <akhilsharma3332@gmail.com>
2021-12-25 09:18:54 +0530

    Bruteforce Written good :)

--- LeetCode/Median of Two Sorted Arrays/SortArrMead.cpp
+++ LeetCode/Median of Two Sorted Arrays/SortArrMead.cpp
@@ -2,59 +2,63 @@
 #include <vector>
 using namespace std;
 
-
+// Brute force 
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        double num1_end = nums1.back();
-        double num2_front = nums2.front();
-        
-        // cout<< "Size of Nums1 = "<< nums1.size() <<endl;
-        // cout<< "Size of Nums2 = "<< nums2.size() <<endl;
+    vector<int> mergeArr(vector<int>& nums1, vector<int>& nums2, vector<int>& merged){
 
-        double med;
-        int mergedSize = nums1.size() +  nums2.size();
+        int n1 = nums1.size(), n2 = nums2.size(), p1 = 0 , p2 = 0;
 
-        if (mergedSize % 2  == 0 ){ 
-            cout<< "Num1 end = "<< num1_end << " | Num2 front = " << num2_front << endl;  
-            med = (num1_end + num2_front) / 2;
-            cout<< "Med = " << med<< endl ;
-
-        }else{
-             
-            int medCountIndex = (mergedSize / 2) ;
-
-            cout<< "medCount = " << medCountIndex <<endl ;
-            cout<< "nums1[medCount] = " << nums1[medCountIndex] <<endl ;
-
-            if (nums1.size() > medCountIndex){
-              cout<< "nums1.size() > mergedSize)" <<endl ;
-              med = 1.0 * nums1[medCountIndex];
-              return med;
+        while (p1 < n1 && p2 < n2){
+            if(nums1[p1] < nums2[p2]){
+                merged.push_back(nums1[p1++]);
+            }else if(nums1[p1] == nums2[p2] ){                
+                merged.push_back(nums1[p1++]);
+                merged.push_back(nums2[p2++]);
+            }else {
+                merged.push_back(nums2[p2++]);
             }
-              med = 1.0 * nums2[medCountIndex - nums1.size()];
-              return med;
-
         }
-        
 
-        return med;
+        while (p1 < n1) merged.push_back(nums1[p1++]);
+        while (p2 < n2) merged.push_back(nums2[p2++]);
+        return merged;
+    }
+
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+        vector<int> merged;
+        mergeArr(nums1, nums2, merged);
+
+        int mid = merged.size() / 2;
+
+        for (auto &&i : merged) cout << i << " "; cout<< "\n";
+    
+        double result;
+        if(merged.size() % 2 == 0){
+            result = (double)(merged[mid] + merged[mid - 1]) /(double)2;
+            return result;
+        }else {
+            result = (double)merged[mid];
+        }
+
+        return result;
     }
 };
 
 int main(){
 
-    vector<int> num1;
-    vector<int> num2;
-
-    num1.push_back(1);
-    num1.push_back(3);
-    
-    num2.push_back(2);
+    vector<int> num1 = {1, 2};
+    vector<int> num2 = {3, 4};
 
     Solution s;
     double returns =  s.findMedianSortedArrays(num1 , num2);
     cout<< "Returned value = " << returns << endl ;
+
+
+    int a = 3 , b = 2;
+    double ba = (double)a/(double)b;
+    cout << ba;
 
    return 0;
 }
