ae648923573ba68bfaa55365673997aa086ebe1a
akhilsharmaa <akhilsharma3332@gmail.com>
2021-12-28 18:27:34 +0530

    kthGrammar solved :)

---
+++ LeetCode/Kth Symbol in Grammar/kthSymbol.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for K-th Symbol in Grammar.
+Memory Usage: 6 MB, less than 39.48% of C++ online submissions for K-th Symbol in Grammar.
+*/
+
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if(n == 1 && k == 1){
+            return 0;
+        }
+
+        int mid = pow(2, n-1)/2;
+
+        if(k <= mid){
+            return kthGrammar(n-1, k);
+        }else{
+            return !kthGrammar(n-1, k-mid);
+        }
+    }
+};
+
+int main(){
+    
+
+    return 0;
+}
