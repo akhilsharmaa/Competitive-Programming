0520b34523168377b07786d9d10d738b95c7c232
akhilsharmaa <akhilsharma3332@gmail.com>
2022-01-13 14:07:37 +0530

    Number of 1 Bits Solved :)

---
+++ LeetCode/Number of 1 Bits/number.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* 
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Number of 1 Bits.
+Memory Usage: 6 MB, less than 47.28% of C++ online submissions for Number of 1 Bits.
+
+*/
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int total = 0;
+
+        for (int i = 0; i < 32; i++){
+            if(n % 2 != 0) total++;
+            n = n >> 1;
+        }
+
+        return total;
+    }
+};
+
+int main(){
+    
+    Solution s ;
+    cout <<  s.hammingWeight(16);
+
+    return 0;
+}
