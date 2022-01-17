1dea422db15274f23d4dad4a2b19b169074a070e
akhilsharmaa <akhilsharma3332@gmail.com>
2022-01-17 09:34:42 +0530

    Subsets solved :)

--- LeetCode/Subset /subset.cpp
+++ LeetCode/Subset /subset.cpp
@@ -1,24 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/* Print the subset of 
-    Input  : {1, 4, 5}
-    Output : [145], [14], [15], [1], [45], [4], [5], [] 
+/* 
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Subsets.
+Memory Usage: 7.1 MB, less than 83.66% of C++ online submissions for Subsets.
 */
 
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-    
 
+    void helper(vector<int>& nums, int i, vector<int> &sets, vector<vector<int>> &result) {
+
+        if(i == nums.size()){
+            result.push_back(sets);
+            return;
+        }
+
+        sets.push_back(nums[i]);
+        helper(nums, i+1, sets, result);
+
+        sets.pop_back();
+        helper(nums, i+1, sets, result);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<vector<int>> result;
+        vector<int> sets;
+        helper(nums, 0, sets, result);
+
+        return result;
     }
 };
 
 
 int main(){
 
-    int arr[] = {1 , 4, 5};
+    vector<int> arr = {1, 2, 3};
+    Solution s;
+    vector<vector<int>> v = s.subsets(arr);
 
+
+    // Print Ouput of Arr-Vector 
+    for (auto && i : v){
+       for (auto && j : i){
+          cout << j << " " ;
+       }cout << "\n" ;
+    }
 
     return 0;
 }
