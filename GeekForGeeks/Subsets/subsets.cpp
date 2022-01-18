f8599ffaa7dca072199fd16582cb1fa238e6b1a2
akhilsharmaa <akhilsharma3332@gmail.com>
2022-01-18 19:00:28 +0530

    / Not Accepted :(

---
+++ GeekForGeeks/Subsets/subsets.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Not Accepted :( 
+class Solution{
+
+    void helper(vector<int>& arr, int i, vector<int>& temp, vector<vector<int>> &result){
+
+        if(i == arr.size()){
+            result.push_back(temp);
+            return;
+        }
+        
+        helper(arr, i+1, temp, result);
+        temp.push_back(arr[i]);
+
+        helper(arr, i+1, temp, result);
+        temp.pop_back();
+    }
+
+    public:
+    vector<vector<int> > subsets(vector<int>& A){
+         vector<vector<int>> result;
+            vector<int> temp;
+         helper(A, 0, temp, result);
+
+         return result;
+    }
+};
+
+
+int main(){
+    
+    Solution s;
+
+    vector<int> arr = {2, 3, 1};
+    vector<vector<int> > v = s.subsets(arr);
+
+    for (auto && i : v) {
+        for (auto && j : i){
+            cout << j <<  ' ';
+        }cout << "\n";
+    }
+    
+
+    return 0;
+}
