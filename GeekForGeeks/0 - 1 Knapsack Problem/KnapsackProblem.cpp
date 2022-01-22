1a8f34961082d745791cb06c56910742e1b55b96
akhilsharmaa <akhilsharma3332@gmail.com>
2022-01-22 19:39:55 +0530

    0 - 1 Knapsack Problem Problem Solved Successfully

--- GeekForGeeks/0 - 1 Knapsack Problem/KnapsackProblem.cpp
+++ GeekForGeeks/0 - 1 Knapsack Problem/KnapsackProblem.cpp
@@ -1,22 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// Problem Solved Successfully 
 class Solution
 {
     public:
+    
+    int t[102][1002];
+
     //Function to return max value that can be put in knapsack of capacity W.
-    int knapSack(int W, int wt[], int val[], int n) {
+    int knapSackHelper(int W, int wt[], int val[], int n) {
 
         if(n == 0|| W == 0){
             return 0;
         }
 
+        if(t[n][W] != -1){
+            return t[n][W];
+        } 
+
         if(wt[n-1] <= W){
-            return max(val[n-1]+ knapSack(W - wt[n-1], wt, val, n-1), 
-                    knapSack(W, wt, val, n-1));
+            return t[n][W] = max(val[n-1]+ knapSackHelper(W - wt[n-1], wt, val, n-1), 
+                    knapSackHelper(W, wt, val, n-1));
         }else
-            return knapSack(W, wt, val, n-1);
+            return t[n][W] = knapSackHelper(W, wt, val, n-1);
     }
+
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int &val[], int n) {
+        
+        memset(t, -1, sizeof(t));
+        return knapSackHelper(W, wt, val, n);
+    }
+
+
+
 };
 
 int main(){
