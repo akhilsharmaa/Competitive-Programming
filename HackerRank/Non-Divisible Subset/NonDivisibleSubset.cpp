14b0be5395e3ca22bdc4493d1c9da289bcf85ee3
akhilsharmaa <akhilsharma3332@gmail.com>
2022-01-25 09:23:48 +0530

    A. Square String? Solved :)

---
+++ HackerRank/Non-Divisible Subset/NonDivisibleSubset.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int nonDivisibleSubset(int k, vector<int> s) {
+    
+        
+}
+
+
+int main(){
+
+//    vector<int> s = {1, 7, 2, 4};
+//    vector<int> s = {19 , 10, 12, 10, 24, 25, 22};
+   vector<int> s = {278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436};
+   int k = 7;
+   cout <<  nonDivisibleSubset(k, s);
+
+   return 0;
+}
--- Other/CodeStudio/Frog Jump/FrogJump.cpp
+++ Other/CodeStudio/Frog Jump/FrogJump.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int helper(int n, vector<int> &heights, int i, int sum){
 
-    if(i < 0) return 0 ;
+    if(i == 0) return 0 ;
     
     
-    int left = abs(heights[i] - heights[i-1]);
+    int left = abs(heights[i] - heights[i-1]) ;
     helper(n, heights, i-1,sum += left);
 
-     int right = INT_MAX;
+    int right = INT_MAX;
 
     if(i > 1){
         right = abs(heights[i] - heights[i-2]);
