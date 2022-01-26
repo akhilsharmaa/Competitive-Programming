ec528d84b18f57560c3fa49ec5ded33446239894
akhilsharmaa <akhilsharma3332@gmail.com>
2022-01-26 09:16:35 +0530

    Solution to the problem B has been submitted successfully

---
+++ CodeForces/Problem Set/Difficulty 800/A_Equal_or_Not_Equal.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pb push_back
+#define MOD 1000000007
+#define vt vector
+#define py cout << "YES" << endl;
+#define pn cout << "NO" << endl;
+
+// Solution to the problem A has been submitted successfully
+
+int main(){
+
+int testCase;
+cin >> testCase;
+   
+   while(testCase--){
+
+      string st;
+      cin >> st;
+
+      ll n = st.length();
+
+      ll count = 0;
+      for (ll i = 0; i < n; i++){
+         if(st[i] == 'N')count ++;
+      }
+      
+      if(count == 1){
+         pn
+      }else 
+         py
+
+   }
+
+   return 0;
+}
