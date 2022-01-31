57509b4320a3fde1cd499252b9e73a75a35eac9d
akhilsharmaa <akhilsharma3332@gmail.com>
2022-01-31 07:20:03 +0530

    Solved ABC Practice

---
+++ CodeForces/Problem Set/Difficulty 800/A_ABC.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pb push_back
+#define MOD 1000000007
+#define vt vector
+#define pYes cout << "YES" << endl;
+#define pNo cout << "NO" << endl;
+
+// *Accepted By CodeForces
+
+void solve(){
+    
+    ll a, b, c, p, q, n, r, k, m, x, y, z;
+    string str;
+
+    cin >> n;
+    cin >> str;
+
+    if(n > 2){
+        pNo
+        return ;
+    }
+
+    if(str == "0" ||
+        str == "10" ||
+        str == "01" ||
+        str == "1" ){
+            pYes
+    }else{
+        pNo
+    }
+    
+    
+}
+
+// *CODE BY AKHILESH 
+// C++ 
+
+int main(){
+
+   int tCase;
+   cin >> tCase;
+   
+   while(tCase--){
+       solve();
+   }
+
+   return 0;
+}
