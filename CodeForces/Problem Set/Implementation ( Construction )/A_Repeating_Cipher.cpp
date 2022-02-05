aca1295aed0a3bd113ae44aaa454e6fed493ea33
akhilsharmaa <akhilsharma3332@gmail.com>
2022-02-05 17:22:18 +0530

    1095A - Repeating Cipher ACCEPTED :)

---
+++ CodeForces/Problem Set/Implementation ( Construction )/A_Repeating_Cipher.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pb push_back
+#define MOD 1000000007
+#define vt vector
+#define pYes cout << "YES" << endl;
+#define pNo cout << "NO" << endl;
+
+// 1095A - Repeating Cipher ACCEPTED :) 
+
+void solve(){
+
+   string str; 
+   cin >> str;
+   int n = str.length();
+
+   string res = "";
+
+    int i = 0, count = 1;
+    while(i < n){
+        res += str[i]; 
+        i+= count; 
+        count++;
+    }
+   
+    cout << res;
+}
+
+
+int main(){
+
+ // For Faster Output
+   ios_base:: sync_with_stdio(false);
+   cin.tie(NULL);
+
+   int tCase;
+   cin >> tCase;
+   
+   while(tCase--){
+       solve();
+   }
+
+   return 0;
+}
+
+/* 
+   AUTHOR- 🇦​​​​​🇰​​​​​🇭​​​​​🇮​​​​​🇱​​​​​🇪​​​​​🇸​​​​​🇭​​​​​
+   ID - @akhilsharmaa
+*/
+
