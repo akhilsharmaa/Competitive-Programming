202950e1e5f721a6bf3fc46bd4e3a18bcd33827c
akhilsharmaa <akhilsharma3332@gmail.com>
2022-02-14 22:04:22 +0530

    959A - Mahmoud and Ehab and the even-odd game ACCEPTED

---
+++ CodeForces/Problem Set/Mathematics /A_Mahmoud_and_Ehab_and_the_even-odd_game.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pb push_back
+#define MOD 1000000007
+#define vt vector
+#define pYes cout << "YES" << endl; 
+#define pNo cout << "NO" << endl; 
+
+// 	959A - Mahmoud and Ehab and the even-odd game ACCEPTED 
+
+void solve(){
+    
+    ll n;cin >> n;
+
+    if(n % 2 == 0)
+        cout << "Mahmoud";
+    else 
+        cout << "Ehab";
+    
+}
+
+
+int main(){
+
+ // For Faster Output
+   ios_base:: sync_with_stdio(false);
+   cin.tie(NULL);
+
+    solve();
+
+   return 0;
+}
+
+/* 
+   AUTHOR- 🇦​​​​​🇰​​​​​🇭​​​​​🇮​​​​​🇱​​​​​🇪​​​​​🇸​​​​​🇭​​​​​
+   ID - @akhilsharmaa
+*/
+
