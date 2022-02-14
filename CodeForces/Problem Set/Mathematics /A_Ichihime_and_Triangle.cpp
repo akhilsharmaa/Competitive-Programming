0f3b4051b127a7e70376fe302e4914f907be2068
akhilsharmaa <akhilsharma3332@gmail.com>
2022-02-14 12:21:54 +0530

    1337A - Ichihime and Triangle ACCEPTED SOLUTION

---
+++ CodeForces/Problem Set/Mathematics /A_Ichihime_and_Triangle.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pb push_back
+#define MOD 1000000007
+#define vt vector
+#define pYes cout << "YES" << endl; 
+#define pNo cout << "NO" << endl; 
+
+// 1337A - Ichihime and Triangle ACCEPTED :) 
+
+void solve(){
+    
+   ll a, b, c, d;
+   cin >> a >> b >> c >> d;
+
+   cout << b << " " << c << " " << c << "\n" ;
+
+}
+
+
+int main(){
+
+ // For Faster Output
+   ios_base:: sync_with_stdio(false);
+   cin.tie(NULL);
+
+   int tCase;
+   cin >> tCase;
+   
+   while(tCase--){
+       solve();
+   }
+
+   return 0;
+}
+
+/* 
+   AUTHOR- AKHILESH 
+   ID - @akhilsharmaa
+*/
+
