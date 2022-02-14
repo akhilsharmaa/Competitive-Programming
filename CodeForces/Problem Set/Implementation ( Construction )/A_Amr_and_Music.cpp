26451f0fcc59089e76ae61c3c9f4f4da2c774306
akhilsharmaa <akhilsharma3332@gmail.com>
2022-02-14 12:03:46 +0530

    NOT SOLVED

---
+++ CodeForces/Problem Set/Implementation ( Construction )/A_Amr_and_Music.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pb push_back
+#define MOD 1000000007
+#define vt vector
+#define pYes cout << "YES" << endl; 
+#define pNo cout << "NO" << endl; 
+
+
+void solve(){
+    
+    // Taking INPUT Array
+    ll n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    cin >> arr[i]; // input-values
+    
+    sort(arr.begin(), arr.end());
+
+    vt<int> res;
+    ll sum = 0, days =0;
+
+    for (int i = 0; i < n; i++){
+        if(k >= (sum + arr[i])){
+            sum+= arr[i];
+            days++;
+            res.pb(arr[i]);
+        }
+    }
+
+    cout << days << "\n";
+    
+    // Print Ouput of Arr-Vector 
+    for (auto && i : res){
+       cout << i << " " ;
+    }
+    
+}
+
+
+int main(){
+
+ // For Faster Output
+   ios_base:: sync_with_stdio(false);
+   cin.tie(NULL);
+       solve();
+
+
+   return 0;
+}
+
+/* 
+   AUTHOR- 🇦​​​​​🇰​​​​​🇭​​​​​🇮​​​​​🇱​​​​​🇪​​​​​🇸​​​​​🇭​​​​​
+   ID - @akhilsharmaa
+*/
+
