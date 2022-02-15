eb124eebfdccf3c703f24977a7676c9b014cfb93
akhilsharmaa <akhilsharma3332@gmail.com>
2022-02-15 06:13:19 +0530

    1426A - Floor Number

---
+++ CodeForces/Problem Set/Mathematics /A_Floor_Number.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pb push_back
+#define MOD 1000000007
+#define vt vector
+#define pYes cout << "YES" << endl; 
+#define pNo cout << "NO" << endl; 
+
+// 1426A - Floor Number ACCEPTED 
+
+void solve(){
+    
+   ll n, x;
+   cin >> n >> x;
+   
+   if(n < 2){
+      cout << 1 <<  "\n"; return;
+   }
+
+   n -= 2;
+   ll res = (n/x)+2;
+   if(n%x == 0)res--;
+   cout << res << "\n";
+}
+
+
+int main(){
+
+ // For Faster Output
+   ios_base:: sync_with_stdio(false);
+   cin.tie(NULL);
+
+   int tCase;
+   cin >> tCase;
+   
+   while(tCase--){
+       solve();
+   }
+
+   return 0;
+}
+
+/* 
+   AUTHOR- 🇦​​​​​🇰​​​​​🇭​​​​​🇮​​​​​🇱​​​​​🇪​​​​​🇸​​​​​🇭​​​​​
+   ID - @akhilsharmaa
+*/
+
