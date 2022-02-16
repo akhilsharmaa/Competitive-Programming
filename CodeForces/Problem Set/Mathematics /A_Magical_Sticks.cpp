b977113dcdcfd43c127c4fc7dc461a9b36438998
akhilsharmaa <akhilsharma3332@gmail.com>
2022-02-16 19:29:53 +0530

    1371A - Magical Sticks ACCEPTED :)

---
+++ CodeForces/Problem Set/Mathematics /A_Magical_Sticks.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pb push_back
+#define MOD 1000000007
+#define vt vector
+#define pYes cout << "YES" << endl; 
+#define pNo cout << "NO" << endl; 
+
+// 1371A - Magical Sticks ACCEPTED :) 
+
+void solve(){
+    
+   ll n; cin >> n;
+   
+   ll res = n / 2;
+   if(n % 2 != 0){
+       res++;
+   }
+    
+   cout << res << endl; 
+}
+
+
+int main(){
+
+ // For Faster Output
+   ios_base:: sync_with_stdio(false);
+   cin.tie(NULL);
+
+   int tCase;
+   cin >> tCase;
+   
+   while(tCase--){
+       solve();
+   }
+
+   return 0;
+}
+
+/* 
+   Author - @akhilsharmaa
+   C++ code by Akhilesh Sharma
+*/
+
