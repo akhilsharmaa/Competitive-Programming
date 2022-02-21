467021e64a84508f8dd8a18cd8dda558147cc786
akhilsharmaa <akhilsharma3332@gmail.com>
2022-02-21 10:57:01 +0530

    1199B - Water Lily

---
+++ CodeForces/Problem Set/Mathematics /B_Water_Lily.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pb push_back
+#define MOD 1000000007
+#define vt vector
+#define pYes cout << "YES" << endl; 
+#define pNo cout << "NO" << endl; 
+
+// 1199B - Water Lily ACCEPTED :) 
+
+void solve(){
+    
+    double H, L; 
+    cin >> H >> L;
+    cout << fixed << setprecision(13) << ((L * L - H * H) / (2 * H));
+    
+}
+
+
+int main(){
+
+ // For Faster Output
+   ios_base:: sync_with_stdio(false);
+   cin.tie(NULL);
+
+       solve();
+   
+
+   return 0;
+}
+
+/* 
+   Author - @akhilsharmaa
+   C++ code by Akhilesh Sharma
+*/
+
