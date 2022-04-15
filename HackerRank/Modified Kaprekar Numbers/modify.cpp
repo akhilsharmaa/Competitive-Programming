dc222ea2e149db10f472ff7d23f0ab9bb8b16286
akhilsharmaa <akhilsharma3332@gmail.com>
2022-04-15 13:16:04 +0530

    Error in 4 TestCase

---
+++ HackerRank/Modified Kaprekar Numbers/modify.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+// Error in 4 TestCase
+bool isKaprekar(ll num, string numStr, ll sqr){
+
+    string lStr = "0";
+    string rStr = to_string(sqr) + "";
+
+    //! EDGE 
+    if(num % 10 == 0  )return false;
+
+    for (int i = 0; i <= numStr.length() + 1; i++){
+
+        int rSum = stoi(rStr), lSum = stoi(lStr);
+
+        // cout << rSum << ' ' << lSum << endl;
+        if(rSum + lSum == num){
+            return 1;
+        }
+
+        lStr.push_back(rStr[i]);
+        rStr[i] = '0';
+    }
+
+    // cout << lStr << ' ' << rStr << endl;
+
+    return false;
+}
+
+int main(){
+
+    int p, q;
+    cin >> p >> q;
+
+    for (ll i = p; i <= q; i++){
+        string numStr = to_string(i);
+        if(isKaprekar(i, numStr, i * i)){
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
