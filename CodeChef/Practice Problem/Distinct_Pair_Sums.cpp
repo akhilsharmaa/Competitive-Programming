8269a09ca4535d4e85000c82466eb9fa451b51c3
akhilsharmaa <akhilsharma3332@gmail.com>
2022-07-01 15:18:27 +0530

    Solved :)

---
+++ CodeChef/Practice Problem/Distinct_Pair_Sums.cpp
@@ -0,0 +1,42 @@
+/* 
+   Author - @akhilsharmaa
+   C++ code by Akhilesh Sharma
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll           long long int
+#define vt           vector
+#define pb           push_back
+#define pNo          cout<<"NO"<<endl;
+#define pYes         cout<<"YES"<<endl;
+#define loop(i,a,b)  for(int i=(a);i<=b;++i)
+#define MOD          100000009
+#define all(V)       (V).begin(),(V).end()
+#define printv(v)    for(auto&&i:v)cout<<i<<' ';
+
+
+void solve(){
+    
+   ll l, r;
+   cin >> l >> r;
+
+   ll ans = 2*(r-l)+1;
+   cout << ans << endl; 
+}
+
+
+int main(){
+
+   // Faster I/O
+   cin.tie(NULL);
+   ios_base::sync_with_stdio(false);
+
+   int t = 0;
+   cin >> t ;
+   while(t--){
+       solve();
+   }
+   return 0;
+}
+
