69a3e1a00efce9d771b81da6891b6c7a1482d773
Akhilesh Sharma <akhilsharma3332@gmail.com>
2022-09-10 22:33:58 +0530

    Solved

--- CSES Problemset/Dynamic Programming/Minimizing Coins/Mini.cpp
+++ CSES Problemset/Dynamic Programming/Minimizing Coins/Mini.cpp
@@ -1,43 +1,50 @@
-/* 
-   Author - @akhilsharmaa
-   C++ code by Akhilesh Sharma
-*/
+/* Akhilesh Sharma C++ */
 
 #include <bits/stdc++.h>
 using namespace std;
 
 #define ll           long long int
 #define vt           vector
-#define pb           push back
-#define no           cout<<"NO"<<endl;
-#define yes          cout<<"YES"<<endl;
-#define loop(i,a,b)  for(int i=(a);i<=b;++i)
-#define all(V)       (V).begin(),(V).end()
-#define printv(v)    for(auto&&i:v)cout<<i<<' ';
+#define pb           push_back
+#define endl           "\n";    
+#define printv(v)    for(auto&&i:v)cerr<<i<<' ';cerr<<endl;
+#define fastIO       cin.tie(NULL);ios_base::sync_with_stdio(false);
+#define debug(n)     cerr <<#n<<" = "<<n<<endl;
+#define all(v)          v.begin(), v.end()
+#define MOD          1e9 + 7
 
 
-void solve(){
+void solve(){ 
+            
+    int n, k; 
+    cin >> n >> k;
+    vector<int> v(n),  dp(k+1, 1e9);
+    
+    for(int i=0; i < n; ++i){
+       cin >> v[i];
+    }
+    
+    dp[0] = 0;
+    
+    for (int i = 1; i <= k; ++i){
+        for (int j = 0; j < n; ++j){
+            if(i - v[j] >= 0){
+                dp[i] = min(dp[i], dp[i-v[j]]+1);
+            }
+        }
 
-   ll n;
-   cin >> n;
-
-   vector<ll> dp(n+1, 0);
-   dp[0] = 
-
+    }
+    
+    cout << (dp[k] == 1e9 ? -1 : dp[k]) << endl;
+        // printv(dp);
 }
+
 
 int main(){
 
-   speed // Faster I/O
+  fastIO 
+  solve();
 
-   int t = 1; 
-   
-
-   while(t--){
-     solve();
-     
-   }
-
-   return 0;
+  return 0;
 }
 
