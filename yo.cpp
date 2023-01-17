6b2f13f2bf3b37869fdbd8d13af2ef0b75b4b2eb
Akhilesh Sharma <74103314+akhilsharmaa@users.noreply.github.com>
2023-01-17 08:25:28 -0800

    Update yo.cpp

--- yo.cpp
+++ yo.cpp
@@ -1,61 +1,52 @@
-/* Akhilesh Sharma C++ */
+// by @akhilsharmaa 
 
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long int ll;
-typedef long double ld;
-
 #define pb           push_back
 #define endl         "\n";  
-#define scanVt(v,n)  for(int i=0;i<n;++i)cin >> v[i];
-#define printVt(v)   for(auto&&i:v)cout<<i<<' ';cout<<"\n";
-#define yN(ok)       cout << (ok? "YES": "NO") << endl;
-#define debugln(n)   cerr <<#n<<":"<<n<<endl;
-#define debug(n)     cerr <<#n<<":"<<n<<' ';
+#define int          long long int
+#define ff           first 
+#define ss           second 
 
-const string upAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowAlpha = "abcdefghijklmnopqrstuvwxyz";
-const ll MOD = 1e9 + 7;
+#define printV(v)     for(auto&&i:v)cout<<i<<' ';cout<<"\n";
+#define debugV(v)     for(auto&&i:v)cerr<<i<<' ';cerr<<"\n";
+#define scanV(v)      for(auto&&i:v)cin >> i;
+#define yN(ok)        cout << (ok? "YES": "NO") << endl;
+#define p(n)          cout << n << ' ';
+#define pln(n)        cout << n << endl;
+#define debugln(n)    cerr <<#n<<":"<<n<<endl;
+#define debug(n)      cerr <<#n<<":"<<n<<' ';
 
 
+const int MOD = 1e9 + 7;
 
 
-void solve(){ 
+void solve(int T){ 
 
-    int n;
-    string str;
-     
-    cin >> n >> str;
     
-    int zero = n; 
-    for (int i = n-1; str[i] != '\0'; --i){
-      if(str[i] == '0'){
-        zero--;
-      }else break;
-    }
-    
-    cout << zero << endl;
+
 }
 
 
-int main(){
+signed main(){
   
   cin.tie(NULL);
   ios_base::sync_with_stdio(false);
-    
+  
+  int T = 1;
+  
   #ifndef ONLINE_JUDGE
-  freopen("inputf.in","r",stdin);
-  freopen("outputf.in","w",stdout);
-  freopen("errorf.in","w",stderr);
+    freopen("inputf.in","r",stdin);
+    freopen("outputf.in","w",stdout);
+    // freopen("errorf.in","w",stderr);
   #endif
   
-  int t = 1;
-  cin >> t;
   
-  while(t--)
-  {
-    solve();
+  cin >> T;
+  
+  for(int t = 1; t <= T;t++){
+    solve(t);
   }
 
   return 0;
